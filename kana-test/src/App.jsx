da2b5579a9cc0709bc9206032b4309e82698da13
Sebastien Colbert <colbertsebastien@gmail.com>
2022-05-28 19:25:16 +0200

    📦 NEW: Ajout de pages

---
+++ kana-test/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Hiragana from './pages/Hiragana'
+import NoPage from './pages/NoPage';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='hiragana' element={<Hiragana />} />
+          <Route path='*' element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
