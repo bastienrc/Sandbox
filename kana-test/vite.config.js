6bb54f226ef8f8af914290cc89bc1475cf2c38ef
Sebastien Colbert <colbertsebastien@gmail.com>
2022-05-28 19:03:09 +0200

    👌 IMPROVE: update configuration

--- kana-test/vite.config.js
+++ kana-test/vite.config.js
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  esbuild: {
+    loader: 'jsx',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
