9ee44d8a437f5e9243771d53e75de810b85156f1
Sebastien Colbert <colbertsebastien@gmail.com>
2022-07-31 10:30:43 +0200

    📦 NEW: Test 2

--- sandbox/src/components/test-002.tsx
+++ sandbox/src/components/test-002.tsx
@@ -1,9 +1,25 @@
 import { useState } from 'react';
 
 export default function Test002() {
+  const [input, setInput] = useState("")
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    images: [""]
+  })
+
+  // setUser({
+  //   name: "john",
+  //   email: "john@gmail.com",
+  //   images: ["profile.png", "cover.png"]
+  // })
+
   return (
     <div className='test'>
       <h2>Test 002: React useState white screen problem fixed</h2>
+      <p>user : <input type="text" placeholder='enter a new name' /></p>
+      <button>Change username</button>
+      <span>Username is {user.name}</span>
       <hr />
     </div>
   );
