dc73d83f12685ca389edb2c874c8c807566c853d
Sebastien Colbert <colbertsebastien@gmail.com>
2022-10-11 13:06:20 +0000

    🚀 RELEASE: Ajout des fonctions au test 2

--- sandbox/src/components/test-002.tsx
+++ sandbox/src/components/test-002.tsx
@@ -3,22 +3,27 @@ import { useState } from 'react';
 export default function Test002() {
   const [input, setInput] = useState("")
   const [user, setUser] = useState({
-    name: "",
-    email: "",
-    images: [""]
+    name: "john",
+    email: "john@gmail.com",
+    images: ["profile.png", "cover.png"]
   })
 
-  // setUser({
-  //   name: "john",
-  //   email: "john@gmail.com",
-  //   images: ["profile.png", "cover.png"]
-  // })
+  const changeUser = () => {
+    setUser((prev) => ({ ...prev, name: input }))
+  }
 
   return (
     <div className='test'>
       <h2>Test 002: React useState white screen problem fixed</h2>
-      <p>user : <input type="text" placeholder='enter a new name' /></p>
-      <button>Change username</button>
+      <p>
+        user :
+        <input
+          type="text"
+          placeholder='enter a new name'
+          onChange={e => setInput(e.target.value)}
+        />
+      </p>
+      <button onClick={changeUser}>Change username</button>
       <span>Username is {user.name}</span>
       <hr />
     </div>
